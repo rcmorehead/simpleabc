a0ae76e00bf8dfe6a253247b219e5574031ffcf0
Robert Morehead <rmorehead@astro.ufl.edu>
2014-04-21 22:03:56 -0400

    began refactoring simplest model to follow new paradigm

--- simplest_model.py
+++ simplest_model.py
@@ -1,15 +1,32 @@
 import model
 from scipy import stats
 import numpy as np
+from simple_lib import *
 
+class MyModel(model.Model):
 
-class Model(model.PlanetsModel):
+        def draw_theta(self):
+            binom_n = stats.randint.rvs(1, 10, 1)
+            scale = stats.uniform.rvs(0, 10, 1)
+
+            return binom_n, scale
+
+        def generate_data(self, theta):
+            pass
+
+        def summary_stats(self, data):
+            transits = np.where((1.0 > data['b']) & (data['b'] > -1.0))
+            pcount = np.bincount(data['ktc_kepler_id'][transits])
+            pcount = pcount[np.where(pcount > 0)]
+            bs = data['b'][transits]
+
+            return pcount, bs
+
+        def distance_function(self, summary_stats, summary_stats_synth):
+            return stats.ks_2samp(summary_stats, summary_stats_synth)[0]
 
         def planets_per_system(self, n, size):
             return stats.binom.rvs(n, .5, size=size)
-
-        #def planet_b(self, n, sigma):
-        #    return stats.norm.rvs(scale=sigma, size=n)
 
         def planet_period(self, size):
             return 10**stats.uniform.rvs(0, 3, size=size)
@@ -24,7 +41,4 @@ class Model(model.PlanetsModel):
             return stats.rayleigh.rvs(scale, size=size)
 
 
-class ABC(model.ABC):
 
-        def distance_function(self, simulation, observed):
-            return stats.ks_2samp(simulation, observed)[0]
