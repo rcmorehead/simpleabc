ac7934fffad90483a40753d8e63dfb1f186a9d32
Robert Morehead <rcm242@psu.edu>
2015-10-18 21:25:32 -0400

    trying to get 1-d to work. Also renormalized sampling weight for random choice.

--- simple_abc.py
+++ simple_abc.py
@@ -203,13 +203,13 @@ def basic_abc(model, data, epsilon=1, min_samples=10,
                     trial_count += 1
 
             attempts = int(float(trial_count)/float(accepted_count + 1) *
-                        (min_samples  - accepted_count))
+                        (min_samples - accepted_count))
 
         return (posterior, distances,
                 accepted_count, trial_count,
                 epsilon)
     else:
-        while accepted_count <= min_samples :
+        while accepted_count <= min_samples:
 
             trial_count += 1
 
@@ -227,7 +227,7 @@ def basic_abc(model, data, epsilon=1, min_samples=10,
 
                 theta_star = theta_prev[:, np.random.choice(
                                         xrange(0, theta_prev.shape[1]),
-                                        replace=True, p=weights)]
+                                        replace=True, p=weights/weights.sum())]
 
                 theta = stats.multivariate_normal.rvs(theta_star, tau_squared)
 
@@ -434,43 +434,67 @@ def calc_weights(theta_prev, theta, tau_squared, weights, prior="None"):
     Calculates importance weights
     """
     weights_new = np.zeros_like(weights)
-    norm = np.zeros(theta_prev.shape[1])
-    for i in xrange(theta.shape[1]):
-        prior_prob = np.zeros(theta[:, i].size)
-        for j in xrange(theta[:, i].size):
-            print theta[:, i][j]
-            prior_prob[j] = prior[j].pdf(theta[:, i][j])
-        #assumes independent priors
-        p = prior_prob.prod()
 
-        for j in xrange(theta_prev.shape[1]):
-            norm[j] = stats.multivariate_normal.pdf(theta[:, i],
-                                                    mean=theta_prev[:, j],
-                                                    cov=tau_squared)
+    if len(theta.shape) == 1:
+        norm = np.zeros_like(theta)
+        for i, T in enumerate(theta):
+            for j in xrange(theta_prev[0].size):
+                #print T, theta_prev[0][j], tau_squared
+                #print type(T), type(theta_prev), type(tau_squared)
+                norm[j] = stats.norm.pdf(T, loc=theta_prev[0][j],
+                                     scale=tau_squared)
+            weights_new[i] = prior[0].pdf(T)/sum(weights * norm)
 
-        weights_new[i] = p/sum(weights * norm)
+        return weights_new/weights_new.sum()
 
-    return weights_new/weights_new.sum()
+    else:
+        norm = np.zeros(theta_prev.shape[1])
+        for i in xrange(theta.shape[1]):
+            prior_prob = np.zeros(theta[:, i].size)
+            for j in xrange(theta[:, i].size):
+                #print theta[:, i][j]
+                prior_prob[j] = prior[j].pdf(theta[:, i][j])
+            #assumes independent priors
+            p = prior_prob.prod()
+
+            for j in xrange(theta_prev.shape[1]):
+                norm[j] = stats.multivariate_normal.pdf(theta[:, i],
+                                                        mean=theta_prev[:, j],
+                                                        cov=tau_squared)
+
+            weights_new[i] = p/sum(weights * norm)
+
+        return weights_new/weights_new.sum()
 
 def weighted_covar(x, w):
     """
 
-    :param x: 2 dimensional array-like, values
+    :param x: 1 or 2 dimensional array-like, values
     :param w: 1 dimensional array-like, weights
-    :return C: Weighted covariance of x
+    :return C: Weighted covariance of x or weighted variance if x is 1d
     """
     sumw = w.sum()
     assert_almost_equal(sumw, 1.0)
-    assert x.shape[1] == w.size
+    if len(x.shape) == 1:
+        assert x.shape[0] == w.size
+    else:
+        assert x.shape[1] == w.size
     sum2 = np.sum(w**2)
-    xbar = [(w*x[i]).sum() for i in xrange(x.shape[0])]
-    covar = np.zeros((x.shape[0], x.shape[0]))
-    for k in xrange(x.shape[0]):
-        for j in xrange(x.shape[0]):
-            for i in xrange(x.shape[1]):
-                covar[j,k] += (x[j,i]-xbar[j])*(x[k,i]-xbar[k]) * w[i]
 
-    return covar * sumw/(sumw*sumw-sum2)
+
+    if len(x.shape) == 1:
+        xbar = (w*x).sum()
+        var = sum(w * (x - xbar)**2)
+        return var * sumw/(sumw*sumw-sum2)
+    else:
+        xbar = [(w*x[i]).sum() for i in xrange(x.shape[0])]
+        covar = np.zeros((x.shape[0], x.shape[0]))
+        for k in xrange(x.shape[0]):
+            for j in xrange(x.shape[0]):
+                for i in xrange(x.shape[1]):
+                    covar[j,k] += (x[j,i]-xbar[j])*(x[k,i]-xbar[k]) * w[i]
+
+        return covar * sumw/(sumw*sumw-sum2)
 
 def effective_sample_size(w):
     '''
